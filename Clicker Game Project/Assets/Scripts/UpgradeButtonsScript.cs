cb381f9beced23fafeebfc7a5cdf25489febab8a
Buggyes <89547163+Buggyes@users.noreply.github.com>
2021-08-28 18:23:00 -0300

    Uploading the Clicker Game Project to my GitHub

---
+++ Clicker Game Project/Assets/Scripts/UpgradeButtonsScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeButtonsScript : MonoBehaviour
+{
+	public void AutoClickButton()
+	{
+		FindObjectOfType<_GameManager>().UpgradeAutoClick();
+	}
+	public void ClickValueButton()
+	{
+		FindObjectOfType<_GameManager>().UpgradeClickValue();
+	}
+	public void ClickMultiplierButton()
+	{
+		FindObjectOfType<_GameManager>().UpgradeClickMultiplier();
+	}
+	public void HealthPointsButton()
+	{
+		FindObjectOfType<_GameManager>().UpgradeHealthPoints();
+	}
+}
---
+++ Clicker Game Project/Assets/Scripts/_GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _GameManager : MonoBehaviour
+{
+    public float autoClick, clickValue, clickMult, hp;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void UpgradeAutoClick()
+    {
+        print("Ok Auto Click");
+    }
+    public void UpgradeClickValue()
+    {
+        print("Ok Click Value");
+    }
+    public void UpgradeClickMultiplier()
+    {
+        print("Ok Click Multiplier");
+    }
+    public void UpgradeHealthPoints()
+    {
+        print("Ok Health Points");
+    }
+}
