2d1a82ac1477f0fe83272f798cc7330948a67026
Buggyes <89547163+Buggyes@users.noreply.github.com>
2021-10-15 09:31:37 -0300

    Added the Lag to the game and cleaned up some scripts

--- Projeto de PI/Assets/Scripts/Enemies/Flying Enemy/FlyingEnemyMobility.cs
+++ Projeto de PI/Assets/Scripts/Enemies/Flying Enemy/FlyingEnemyMobility.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoroninhaVoandoMobility : MonoBehaviour
+public class FlyingEnemyMobility : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D enRig;
     [SerializeField] private float limit, speed;
     void Update()
     {
         enRig.velocity = new Vector2(speed, 0);
-        if(enRig.position.x <= limit)
-		{
+        if (enRig.position.x <= limit)
+        {
             Destroy(gameObject);
-		}
+        }
     }
 }
